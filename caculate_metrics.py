02d8f290992720684b3c5cde8fb5c5f04326d0ed
Haoshuai Xu <hsh.xu@outlook.com>
2023-05-29 14:36:55 +0800

    修正计算 f1 值函数没有换行的 bug

--- caculate_metrics.py
+++ caculate_metrics.py
@@ -6,4 +6,4 @@ def caculate_output_metrics(y_true:list, y_pred:list, epoch:int):
     rec = recall_score(y_true, y_pred, average='macro')
     f1 = f1_score(y_true, y_pred, average='macro')
     with open("./result_log/result_log.tsv", "+a") as f:
-        f.writelines(f"{epoch}\t{pre}\t{rec}\t{f1}")
+        f.writelines(f"{epoch}\t{pre}\t{rec}\t{f1}\n")
