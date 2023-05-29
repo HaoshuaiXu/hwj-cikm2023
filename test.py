4c56521121d741eb7d671a17ac4fe8eb88c440c8
Haoshuai Xu <hsh.xu@outlook.com>
2023-05-29 14:27:15 +0800

    test 函数适配计算 f1 值的函数

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import torch
 from torch.utils.data import DataLoader
+import numpy as np
 
 
 def test(
@@ -9,10 +10,13 @@ def test(
     device:str,
     test_mode=False
 ):
+    """返回 [[y_ture], [y_pred]]"""
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     test_loss, correct = 0, 0
     model.eval()
+    true_y_list = []
+    pred_y_list = []
     # 在测试集上运行
     with torch.no_grad():
         for _, data in enumerate(dataloader):
@@ -23,6 +27,8 @@ def test(
             pred = model(ids, mask, token_type_ids)
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+            true_y_list.extend(np.array(y.to('cpu')))
+            pred_y_list.extend(np.array(pred.argmax(1).to('cpu')))
             if test_mode == False:
                 pass
             else:
@@ -30,3 +36,4 @@ def test(
     test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    return true_y_list, pred_y_list
